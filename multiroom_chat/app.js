ea7506e52de41136d7c497cb23194ba9325c90b1
pauloh04 <pauloh04@gmail.com>
2019-03-04 17:08:48 -0300

    Config websocket

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -1,5 +1,15 @@
 var app = require('./config/server');
 
-app.listen(3000, function(){
+var server = app.listen(80, function(){
     console.log('SERVER ON');
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    });
 });
