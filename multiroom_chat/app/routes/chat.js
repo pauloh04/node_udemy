2076a98582ea30feea372f1648289a13879456c3
pauloh04 <pauloh04@gmail.com>
2019-03-04 16:46:56 -0300

    Adding assets / validation

---
+++ multiroom_chat/app/routes/chat.js
@@ -0,0 +1,5 @@
+module.exports = function(app){
+    app.post('/chat', function(req, res){
+        app.app.controllers.chat.iniciaChat(app, req, res);
+    })
+}
--- multiroom_chat/app/routes/index.js
+++ multiroom_chat/app/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
     app.get('/', function(req, res){
-        res.send('Teste');
+        app.app.controllers.index.home(app, req, res);
     })
 }
