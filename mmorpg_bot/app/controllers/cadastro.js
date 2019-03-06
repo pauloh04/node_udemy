cb76714f059dcf69424947da192727b2f14e70d9
pauloh04 <pauloh04@gmail.com>
2019-03-06 17:02:34 -0300

    Init project MMORPG GOT

---
+++ mmorpg_bot/app/controllers/cadastro.js
@@ -0,0 +1,21 @@
+module.exports.cadastro = function(application, req, res){
+    res.render('cadastro', {validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+    var dadosForm = req.body;
+
+    req.assert('nome', 'Nome é obrigatório').notEmpty();
+    req.assert('usuario', 'Usuario é obrigatório').notEmpty();
+    req.assert('senha', 'Senha é obrigatório').notEmpty();
+    req.assert('casa', 'Casa é obrigatório').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('cadastro', {validacao: errors, dadosForm: dadosForm});
+        return;
+    }
+
+    
+}
