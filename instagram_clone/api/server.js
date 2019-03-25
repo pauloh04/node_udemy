b1af8060749964c9e435be6fc97abd179f54b837
pauloh04 <pauloh04@gmail.com>
2019-03-25 14:30:05 -0300

    Impl instagram_clone

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -1,12 +1,22 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    mongodb = require('mongodb');
-    objectId = require('mongodb').ObjectId;
+    mongodb = require('mongodb'),
+    objectId = require('mongodb').ObjectId,
+    connectMultiparty = require('connect-multiparty'),
+    fs = require('fs');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(connectMultiparty());
+app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 var port = 8080;
 
@@ -21,18 +31,34 @@ app.get('/', function(req,res){
 });
 
 app.post('/api', function(req,res){
+    var date = new Date();
 
-    var dados = req.body;
+    var nome_imagem = date.getTime() + '_' + req.files.arquivo.originalFilename;
+    var titulo_imagem = req.body.titulo;
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + nome_imagem;
 
-    db.open(function(err, mongoclient){
-        mongoclient.collection('postagens', function(err, collection){
-            collection.insert(dados, function(err, records){
-                if(err){
-                    res.json(err);
-                } else {
-                    res.json(records);
-                }
-                mongoclient.close();
+    fs.rename(path_origem, path_destino, function(err){
+        if(err){
+            res.status(500).json({error : err});
+            return;
+        }
+
+        var dados = {
+            url_imagem: nome_imagem,
+            titulo: titulo_imagem
+        }
+
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.insert(dados, function(err, records){
+                    if(err){
+                        res.status(500).json(err);
+                    } else {
+                        res.status(200).json({'status': 'foi'});
+                    }
+                    mongoclient.close();
+                });
             });
         });
     });
@@ -73,7 +99,12 @@ app.put('/api/:id', function(req,res){
         mongoclient.collection('postagens', function(err, collection){
             collection.update(
                 {_id : objectId(req.params.id)}, 
-                { $set : { titulo : req.body.titulo }}, 
+                { $push : { 
+                    comentarios : {
+                        id_comentario: new objectId(),
+                        comentario: req.body.comentario
+                    }
+                }}, 
                 {}, 
                 function(err, result){
                     if(err){
@@ -97,11 +128,23 @@ app.delete('/api/:id', function(req,res){
                     if(err){
                         res.json(err);
                     } else {
-                        res.json(result);
+                        res.status(200).json(result);
                     }
                     mongoclient.close();
                 }
             );
         });
+    });
+});
+
+app.get('/uploads/:imagem', function(req,res){
+    var img = req.params.imagem;
+    fs.readFile('./uploads/' + img, function(err, content){
+        if(err){
+            res.status(400).json(err);
+            return;
+        }
+        res.writeHead(200, {'content-type' : 'image/jpg'});
+        res.end(content);
     });
 });
